77fbdfe6f2874c48f61ee3afd5ce7b912dc6189a
AhogeK <ahogek@gmail.com>
2024-12-22 22:12:01 +0800

    style: update all v1ch05 classes

--- src/corejava/v1ch05/abstractClasses/Person.java
+++ src/corejava/v1ch05/abstractClasses/Person.java
@@ -1,17 +1,15 @@
 package abstractClasses;
 
-public abstract class Person
-{
-   public abstract String getDescription();
-   private String name;
+public abstract class Person {
+    private String name;
 
-   public Person(String name)
-   {
-      this.name = name;
-   }
+    public Person(String name) {
+        this.name = name;
+    }
 
-   public String getName()
-   {
-      return name;
-   }
+    public abstract String getDescription();
+
+    public String getName() {
+        return name;
+    }
 }
--- src/corejava/v1ch05/sealed/SealedTest.java
+++ src/corejava/v1ch05/sealed/SealedTest.java
@@ -1,82 +1,83 @@
 package sealed;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-sealed interface JSONValue permits JSONArray, JSONObject, JSONPrimitive
-{
-   public default String type()
-   {
-      if (this instanceof JSONArray) return "array";
-      else if (this instanceof JSONObject) return "object";
-      else if (this instanceof JSONNumber) return "number";
-      else if (this instanceof JSONString) return "string";
-      else if (this instanceof JSONBoolean) return "boolean";
-      else return "null";
-   }
+enum JSONBoolean implements JSONPrimitive {
+    FALSE, TRUE;
+
+    public String toString() {
+        return super.toString().toLowerCase();
+    }
 }
 
-final class JSONArray extends ArrayList<JSONValue>  implements JSONValue {}
+enum JSONNull implements JSONPrimitive {
+    INSTANCE;
 
-final class JSONObject extends HashMap<String, JSONValue> implements JSONValue
-{
-   public String toString()
-   {
-      StringBuilder result = new StringBuilder();
-      result.append("{");
-      for (Map.Entry<String, JSONValue> entry : entrySet())
-      {
-         if (result.length() > 1) result.append(","); 
-         result.append(" \"");
-         result.append(entry.getKey());
-         result.append("\": ");
-         result.append(entry.getValue());
-      }
-      result.append(" }");
-      return result.toString();
-   }
+    public String toString() {
+        return "null";
+    }
+}
+
+sealed interface JSONValue permits JSONArray, JSONObject, JSONPrimitive {
+    public default String type() {
+        if (this instanceof JSONArray) return "array";
+        else if (this instanceof JSONObject) return "object";
+        else if (this instanceof JSONNumber) return "number";
+        else if (this instanceof JSONString) return "string";
+        else if (this instanceof JSONBoolean) return "boolean";
+        else return "null";
+    }
 }
 
 sealed interface JSONPrimitive extends JSONValue
-      permits JSONNumber, JSONString, JSONBoolean, JSONNull
-{   
+        permits JSONNumber, JSONString, JSONBoolean, JSONNull {
 }
 
-final record JSONNumber(double value) implements JSONPrimitive
-{
-   public String toString() { return "" + value; }
+final class JSONArray extends ArrayList<JSONValue> implements JSONValue {
 }
 
-final record JSONString(String value) implements JSONPrimitive
-{
-   public String toString() { return "\"" + value.translateEscapes() + "\""; }
+final class JSONObject extends HashMap<String, JSONValue> implements JSONValue {
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append("{");
+        for (Map.Entry<String, JSONValue> entry : entrySet()) {
+            if (result.length() > 1) result.append(",");
+            result.append(" \"");
+            result.append(entry.getKey());
+            result.append("\": ");
+            result.append(entry.getValue());
+        }
+        result.append(" }");
+        return result.toString();
+    }
 }
 
-enum JSONBoolean implements JSONPrimitive
-{
-   FALSE, TRUE;
-   public String toString() { return super.toString().toLowerCase(); }
+final record JSONNumber(double value) implements JSONPrimitive {
+    public String toString() {
+        return "" + value;
+    }
 }
 
-enum JSONNull implements JSONPrimitive
-{
-   INSTANCE;
-   public String toString() { return "null"; }
+final record JSONString(String value) implements JSONPrimitive {
+    public String toString() {
+        return "\"" + value.translateEscapes() + "\"";
+    }
 }
 
-public class SealedTest
-{
-   public static void main(String[] args)
-   {
-      JSONObject obj = new JSONObject();
-      obj.put("name", new JSONString("Harry"));
-      obj.put("salary", new JSONNumber(90000));
-      obj.put("married", JSONBoolean.FALSE);
-      JSONArray arr = new JSONArray();
-      arr.add(new JSONNumber(13));
-      arr.add(JSONNull.INSTANCE);
-      
-      obj.put("luckyNumbers", arr);
-      System.out.println(obj);
-      System.out.println(obj.type());
-   }
+public class SealedTest {
+    public static void main(String[] args) {
+        JSONObject obj = new JSONObject();
+        obj.put("name", new JSONString("Harry"));
+        obj.put("salary", new JSONNumber(90000));
+        obj.put("married", JSONBoolean.FALSE);
+        JSONArray arr = new JSONArray();
+        arr.add(new JSONNumber(13));
+        arr.add(JSONNull.INSTANCE);
+
+        obj.put("luckyNumbers", arr);
+        System.out.println(obj);
+        System.out.println(obj.type());
+    }
 }
